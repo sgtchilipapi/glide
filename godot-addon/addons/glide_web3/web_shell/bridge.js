d778d397d9ade7eb2d3b40d46b88852b9db0f04b
sgtchilipapi <jayemmanuelg@gmail.com>
2026-04-25 07:00:06 +0800

    Add Glide mock wallet shell methods

--- godot-addon/addons/glide_web3/web_shell/bridge.js
+++ godot-addon/addons/glide_web3/web_shell/bridge.js
@@ -3,11 +3,50 @@
 		return;
 	}
 
+	let loggedIn = false;
+	let walletAddress = "";
+
 	window.glideWallet = {
 		ping: async function () {
 			return {
 				ok: true,
 				source: "shell"
+			};
+		},
+		login: async function () {
+			loggedIn = true;
+			walletAddress = "MOCK_ADDRESS_001";
+			return {
+				ok: true,
+				address: walletAddress,
+				source: "mock_shell"
+			};
+		},
+		logout: async function () {
+			loggedIn = false;
+			walletAddress = "";
+			return {
+				ok: true,
+				source: "mock_shell"
+			};
+		},
+		isLoggedIn: async function () {
+			return {
+				ok: true,
+				logged_in: loggedIn
+			};
+		},
+		getWalletAddress: async function () {
+			return {
+				ok: true,
+				address: walletAddress
+			};
+		},
+		signAndSendTransaction: async function (payload) {
+			return {
+				ok: true,
+				signature: "MOCK_TX_001",
+				request_payload: payload || {}
 			};
 		}
 	};
